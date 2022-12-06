f065cf1b0c9b2d32044273b7eb97e6dc350b8aec
Kiley Hector <kiley.hector@gmail.com>
2022-12-06 16:57:36 -0500

    thank god for my peers

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,6 +1,10 @@
 USE codeup_test_db;
 SELECT * FROM albums;
 SELECT * FROM albums WHERE artist = 'Shania Twain';
-select release_date from album where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+select release_date from albums where name = 'Sgt. Pepper\s Lonely Hearts Club Band';
+select genre from albums where name = 'Nevermind';
+select name from albums where release_date between 1990 and 2000;
+
+
 
 

493b42f96995d8db104726dba87435d9343688a4
Kiley Hector <kiley.hector@gmail.com>
2022-12-07 10:49:57 -0500

    got one part done

--- select_exercises.sql
+++ select_exercises.sql
@@ -4,6 +4,10 @@ SELECT * FROM albums WHERE artist = 'Shania Twain';
 select release_date from albums where name = 'Sgt. Pepper\s Lonely Hearts Club Band';
 select genre from albums where name = 'Nevermind';
 select name from albums where release_date between 1990 and 2000;
+select sales from albums where sales >= 20;
+select sales from albums where sales <= 20;
+select * from albums where genre = 'Rock';
+
 
 
 

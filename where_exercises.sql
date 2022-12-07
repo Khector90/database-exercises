458443f82eb28aeb898e5eef0735fa30511079c3
Kiley Hector <kiley.hector@gmail.com>
2022-12-07 15:00:45 -0500

    got itthank you dustin and jordan

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,8 +1,8 @@
 USE employees;
 
-SELECT emp_no, first_name, last_name
+SELECT emp_no, first_name, last_name, gender
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name in  ('Irena', 'Vidya', 'Maya') and gender = 'M';
 
 
 
@@ -13,3 +13,15 @@ WHERE last_name like 'e%';
 select last_name
 from employees
 where last_name like '%q%';
+
+select last_name, first_name
+from employees
+where last_name like '%e' or  last_name like 'e%';
+
+select last_name, first_name
+from employees
+where last_name like 'e%' and last_name like '%e';
+
+select last_name, first_name
+from employees
+where last_name like '%q%' and last_name not like '%qu%';

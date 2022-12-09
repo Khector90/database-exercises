82f7bcd7e71fafdc15b8470dc2bb3f87c8104de1
Kiley Hector <kiley.hector@gmail.com>
2022-12-09 08:16:44 -0500

    done

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -9,3 +9,22 @@ where last_name like 'e%'
 and last_name like '%e'
 group by last_name
 limit 5;
+
+select distinct last_name, first_name
+from employees
+where last_name like 'e%'
+and last_name like '%e'
+group by last_name, first_name;
+
+select distinct last_name
+from employees
+where last_name like '%q%'
+and last_name not like  '%qu%'
+group by last_name
+order by count(last_name)ASC;
+
+
+
+
+
+
